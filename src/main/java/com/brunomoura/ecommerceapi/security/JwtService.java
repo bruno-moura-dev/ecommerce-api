4c1305b1a5bcf11a2213b66549f1c6f7e5bf73f3
bruno-moura-dev <bruno.moura.backend@gmail.com>
2026-04-09 15:49:30 -0300

    feat(auth): implement JWT service and token handling

---
+++ src/main/java/com/brunomoura/ecommerceapi/security/JwtService.java
@@ -0,0 +1,83 @@
+package com.brunomoura.ecommerceapi.security;
+
+import com.brunomoura.ecommerceapi.config.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final JwtProperties jwtProperties;
+    private final SecretKey signKey;
+
+    public JwtService(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+        this.signKey = Keys.hmacShaKeyFor(jwtProperties.getSecret());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return buildToken(claims, userDetails);
+    }
+
+    public String extractSubject(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+
+        try {
+            return (extractExpiration(token).after(new Date()) || Objects.equals(extractSubject(token),
+                    userDetails.getUsername()));
+
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parserBuilder().setSigningKey(this.signKey).build().parseClaimsJws(token).getBody();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Date getExpirationDate() {
+        return new Date(System.currentTimeMillis() + this.jwtProperties.getExpiration());
+    }
+
+    private String buildToken(Map<String, Object> claims, UserDetails userDetails) {
+
+        return Jwts.builder().
+                setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(getExpirationDate())
+                .signWith(this.signKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
---
+++ src/main/java/com/brunomoura/ecommerceapi/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.brunomoura.ecommerceapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(configurer -> configurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
+
