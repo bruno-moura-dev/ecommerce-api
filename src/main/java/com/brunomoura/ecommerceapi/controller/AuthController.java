56c9c14bb0edb2a411099afdf11c0cddaf8ce4d2
Bruno Moura <moura.bruno2002@gmail.com>
2026-04-09 22:39:17 -0300

    feat(auth): implement complete auth flow

---
+++ src/main/java/com/brunomoura/ecommerceapi/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.brunomoura.ecommerceapi.controller;
+
+import com.brunomoura.ecommerceapi.dto.auth.LoginRequestDTO;
+import com.brunomoura.ecommerceapi.dto.auth.LoginResponseDTO;
+import com.brunomoura.ecommerceapi.dto.user.UserCreateRequestDTO;
+import com.brunomoura.ecommerceapi.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO dto) {
+
+        LoginResponseDTO response = authService.login(dto);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponseDTO> register(@RequestBody @Valid UserCreateRequestDTO dto) {
+
+        LoginResponseDTO response = authService.register(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
