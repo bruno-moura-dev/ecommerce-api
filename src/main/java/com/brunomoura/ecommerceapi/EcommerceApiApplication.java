09ff496fb5842ad7768dbc83c3b93dc5eb80f8b2
bruno-moura-dev <bruno.moura.backend@gmail.com>
2026-04-16 12:45:46 -0300

    feat(bootstrap): implement admin initializer and configuration properties

--- src/main/java/com/brunomoura/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/brunomoura/ecommerceapi/EcommerceApiApplication.java
@@ -1,11 +1,12 @@
 package com.brunomoura.ecommerceapi;
 
+import com.brunomoura.ecommerceapi.config.AdminProperties;
 import com.brunomoura.ecommerceapi.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, AdminProperties.class})
 @SpringBootApplication
 public class EcommerceApiApplication {
 
