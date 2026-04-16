023f243dfff5abc802cda601b2beb4e61075cc56
bruno-moura-dev <bruno.moura.backend@gmail.com>
2026-04-16 12:48:36 -0300

    refactor(config): use Lombok @Getter in JwtProperties

--- src/main/java/com/brunomoura/ecommerceapi/config/JwtProperties.java
+++ src/main/java/com/brunomoura/ecommerceapi/config/JwtProperties.java
@@ -3,6 +3,7 @@ package com.brunomoura.ecommerceapi.config;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -10,6 +11,7 @@ import java.util.Base64;
 
 @ConfigurationProperties(prefix = "jwt")
 @Validated
+@Getter
 public class JwtProperties {
 
     @NotBlank
@@ -26,9 +28,5 @@ public class JwtProperties {
 
     public byte[] getSecret() {
         return Base64.getDecoder().decode(this.secret);
-    }
-
-    public Long getExpiration() {
-        return expiration;
     }
 }
