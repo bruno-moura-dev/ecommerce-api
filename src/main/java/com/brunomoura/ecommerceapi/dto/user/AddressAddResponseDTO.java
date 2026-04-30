9bf11c995f2a15237b9c11ac4b07a696602cdd7f
bruno-moura-dev <bruno.moura.backend@gmail.com>
2026-04-30 17:50:09 -0300

    feat(user): implement AddressCreateDTO to improve clarity and organization

--- src/main/java/com/brunomoura/ecommerceapi/dto/user/AddressAddResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package com.brunomoura.ecommerceapi.dto.user;
-
-public record AddressAddResponseDTO(Long id, String label, String streetName, String houseNumber, String neighborhood,
-                                    String city, String state, String country, String zipCode) {
-}
